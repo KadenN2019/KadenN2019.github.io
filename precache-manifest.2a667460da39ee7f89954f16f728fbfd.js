1dcf98cee676f7c97d29af1528fb7f999a0d96a3
Kaden Neuman <kadenn2019@gmail.com>
2019-12-16 13:48:24 -0800

    added port

---
+++ precache-manifest.2a667460da39ee7f89954f16f728fbfd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1805fa9baf60b13f5710a51fc29b9803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d2ade32f7093391e30",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "e65396270c917f4f8e1f",
+    "url": "/static/css/main.39efc955.chunk.css"
+  },
+  {
+    "revision": "22d2ade32f7093391e30",
+    "url": "/static/js/2.7ec27576.chunk.js"
+  },
+  {
+    "revision": "e65396270c917f4f8e1f",
+    "url": "/static/js/main.77e2eff2.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "1ee2714fa5abd82fbce6021545afe227",
+    "url": "/static/media/DSC09782.1ee2714f.jpg"
+  },
+  {
+    "revision": "274c68bb3daf7b7dce6af23c84b3f1d9",
+    "url": "/static/media/DSC09805.274c68bb.jpg"
+  },
+  {
+    "revision": "9e544f66942b605e9b486eb851625049",
+    "url": "/static/media/MintShoes.9e544f66.jpg"
+  },
+  {
+    "revision": "0c1c14d1b3335707366e39b7aae39601",
+    "url": "/static/media/NeumanKlogo.0c1c14d1.png"
+  },
+  {
+    "revision": "0c5c4208b36fa01209076d1c395638c2",
+    "url": "/static/media/TheViewBar.0c5c4208.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
